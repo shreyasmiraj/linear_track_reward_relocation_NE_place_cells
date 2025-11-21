d27fd341dfd56501336d1b0e708e924b36e0b514
shreyasmiraj <shreyasmiraj8@gmail.com>
2025-11-21 16:28:11 +0530

    refactor

--- helper.h
+++ helper.h
@@ -23,28 +23,136 @@ void eventLog(byte side, byte type, byte state, unsigned long t)
   Serial.println(t);
 }
 
-unsigned long currentTime(bool time_in_microseconds = false)
+unsigned long currentTime(unsigned long tLast = 0, unsigned long tolerance = CLOCK_TOLERANCE, bool time_in_microseconds = false)
 {
   /*
   Get current time in millis() by default
   <bool> time_in_microseconds : set true to get time in micros()
 
-  CAUTION: milli() can go up to 50days before overflow where as
+  CAUTION: milli() can go up to ~49days before overflow where as
   micros() encounters overflow in ~70min
   */
-  return TIME_IN_MICROSECONDS ? micros() : millis();
+  unsigned long tNow = time_in_microseconds ? micros() : millis();
+  while (tNow - tLast > tolerance || tNow < tLast)
+  {
+    tNow = time_in_microseconds ? micros() : millis();
+  }
+  return tNow;
 }
 
-inline bool readIR(byte pin)
+inline bool digitalReadCorrected(byte pin, bool sensorLogicLow = false)
 {
   /*
   Get logic corrected digital read for IR input pin
+  <byte> pin : digital pin ID
+  <bool> sensorLogicLow : true if sensor return low for desired readout
+  returns : <bool> corrected logic response based on sensor output, not of actual read if sensorLogicLow is true
   */
   bool v = digitalRead(pin);
-  return IR_ACTIVE_LOW ? !v : v;
+  return sensorLogicLow ? !v : v;
 }
 
-void initIR(IRState *irDetector, byte pin, byte side)
+void digitalWriteCorrected(byte pin, bool state, bool activeLogicLow = false)
+{
+  /*
+  Write logic corrected value to digital pin
+  <byte> pin : digital pin to write
+  <bool> state : HIGH or LOW, or true or false, or 1 or 0
+  */
+  digital.Write(pin, activeLogicLow ? !state : state);
+}
+
+void initRuntime(RuntimeState &runtimeState, byte pin, unsigned long duration = RUN_TIME_DURATION, unsigned long delay = DELAY_START)
+{
+  /*
+  Initialize default state variable for runtime
+  <struct RuntimeState> runtimeState : runtime struct variable
+  <byte> pin : led indicator pin for runtime - HIGH when on, LOW when off
+  <unsigned long> duration : set total duration for runtime execution, defaults to RUN_TIME_DURATION
+  */
+  runtimeState.led_pin  = pin;
+  runtimeState.runTimeFlag = false;
+  runtimeState.duration = duration;
+  runtimeState.delay = delay;
+  digitalWrite(runtimeState.led_pin, OFF);
+  runtime.tStart = currentTime();
+}
+
+void updateRuntime(RuntimeState &runtimeState)
+{
+  /*
+  Poll for current time and check for start or exit conditions for runtime
+  <struct RuntimeState> runtimeState : runtime struct variable
+  */
+  runtimeState.tNow = currentTime(runtimeState.tLast);
+  
+  //exit condition
+  if (runtimeState.tNow - runtimeState.tRuntimeStart >= runtimeState.duration)
+  {
+    digitalWrite(runtimeState.led_pin, OFF);
+    digitalWriteCorrected(SOLENOID_A_PIN, OFF, SOLENOID_ACTIVE_HIGH);
+    digitalWriteCorrected(SOLENOID_B_PIN, OFF, SOLENOID_ACTIVE_HIGH);
+    runtimeState.runTimeFlag = false;
+
+    // log
+    Serial.print('E');
+    Serial.println(runtimeState.tNow);
+
+    while (true);
+  }
+  
+  //start condition
+  if (!runtimeState.runTimeFlag && runtimeState.tNow - runtimeState.tStart >= DELAY_START)
+  {
+    runtimeState.runTimeFlag = 1;
+    digitalWrite(runtimeState.led_pin, ON);
+    runtimeState.tRuntimeStart = runtimeState.tNow;
+
+    // log
+    Serial.print('S');
+    Serial.println(runtimeState.tRuntimeStart);
+  }
+  runtimeState.tLast = runtimeState.tNow;
+}
+
+void initBlinkLED(BlinkLEDState &ledState, byte pin, byte side, unsigned long blinkInterval = LED_BLINK_INTERVAL)
+{
+  /*
+  Initialize default parameters for blinking LED
+  <struct BlinkLEDState> ledState : struct for led state parameters
+  <byte> side : led location/side identifier
+  <byte> pin : led pin
+  <unsigned long> blinkInterval : blink duration
+  */
+  ledState.pin = pin;
+  ledState.side = side;
+  ledState.ledBlinkState = false; 
+  ledState.blinkInterval = blinkInterval;
+}
+
+void updateBlinkLED(BlinkLEDState &ledState, unsigned long tNow)
+{
+  /*
+  update led blink between on and off
+  <struct BlinkLEDState> ledState : struct for led state parameters
+  <unsigned long> tNow : current time
+  */
+  if (ledState.ledBlinkState && tNow - ledState.tLEDon> ledState.blinkInterval)
+  {
+    digitalWrite(ledState.pin, OFF);
+    ledState.tLEDoff = tNow;
+    ledState.ledBlinkState = false;
+  }
+  if (!ledState.ledBlinkState && tNow - ledState.tLEDon > ledState.blinkInterval)
+  {
+    digitalWrite(ledState.pin, ON);
+    ledState.tLEDon = tNow;
+    ledState.ledBlinkState = true;
+  }
+
+}
+
+void initIR(IRState &irDetector, byte pin, byte side)
 {
   /*
   Init function to initialize irDetector with default parameters
@@ -53,19 +161,19 @@ void initIR(IRState *irDetector, byte pin, byte side)
   <byte> side : side identifier
   */
   pinMode(pin, INPUT_PULLUP);
-  irDetector->pin = pin;
-  irDetector->side = side;
-  irDetector->currentRead = readIR(pin);
-  irDetector->lastRead = false;
-  irDetector->currentPersistant = false;
-  irDetector->lastPersistant = false;
-  irDetector->inBreak = false;
-  irDetector->breakEvent = false;
-  irDetector->breakEventMutable = false;
-  irDetector->connectEvent = true;
+  irDetector.pin = pin;
+  irDetector.side = side;
+  irDetector.currentRead = readCorrected(pin, IR_ACTIVE_LOW);
+  irDetector.lastRead = false;
+  irDetector.currentPersistant = false;
+  irDetector.lastPersistant = false;
+  irDetector.inBreak = false;
+  irDetector.breakEvent = false;
+  irDetector.breakEventMutable = false;
+  irDetector.connectEvent = true;
 }
 
-void detectIR(IRState *irDetector, unsigned long tNow, unsigned long tRuntimeStart)
+void detectIR(IRState &irDetector, unsigned long tNow)
 {
   /*
   Function to detect irDetector state changes and update state parameters accordingly
@@ -77,53 +185,45 @@ void detectIR(IRState *irDetector, unsigned long tNow, unsigned long tRuntimeSta
   <unsigned long> tNow : current time of execution
   <unsigned long> tRuntimeStart : time of runtime start
   */
-  bool v = readIR(irDetector->pin);
-  if ((irDetector->currentRead || irDetector->lastRead) && v && !irDetector->inBreak)
+  bool v = readCorrected(irDetector.pin, IR_ACTIVE_LOW);
+  if ((irDetector.currentRead || irDetector.lastRead) && v && !irDetector.inBreak)
   {
-    irDetector->tStart = tNow;
-    irDetector->inBreak = true;
+    irDetector.tStart = tNow;
+    irDetector.inBreak = true;
   }
-  else if (!(irDetector->currentRead || irDetector->lastRead) && !v && irDetector->inBreak)
+  else if (!(irDetector.currentRead || irDetector.lastRead) && !v && irDetector.inBreak)
   {
-    irDetector->tOff = tNow;
-    irDetector->inBreak = false;
+    irDetector.tOff = tNow;
+    irDetector.inBreak = false;
   }
-  if (tNow - irDetector->tOff >= MIN_IR_BREAK && !irDetector->inBreak && irDetector->breakEvent)
+  if (tNow - irDetector.tOff >= MIN_IR_BREAK && !irDetector.inBreak && irDetector.breakEvent)
   {
-    irDetector->inBreak = false;
-    irDetector->breakEvent = false;
-    irDetector->breakEventMutable = false;
-    irDetector->connectEvent = true;
+    irDetector.inBreak = false;
+    irDetector.breakEvent = false;
+    irDetector.breakEventMutable = false;
+    irDetector.connectEvent = true;
     // log
-    eventLog(irDetector->side, IR, OFF, tNow - tRuntimeStart);
+    eventLog(irDetector.side, IR, OFF, tNow);
   }
-  if (tNow - irDetector->tStart >= MIN_IR_BREAK && irDetector->inBreak && irDetector->connectEvent)
+  if (tNow - irDetector.tStart >= MIN_IR_BREAK && irDetector.inBreak && irDetector.connectEvent)
   {
-    irDetector->breakEvent = true;
-    irDetector->breakEventMutable = true;
-    irDetector->connectEvent = false;
+    irDetector.breakEvent = true;
+    irDetector.breakEventMutable = true;
+    irDetector.connectEvent = false;
     // log
-    eventLog(irDetector->side, IR, ON, tNow - tRuntimeStart);
+    eventLog(irDetector.side, IR, ON, tNow);
   }
-  if (irDetector->breakEventMutable)
+  if (irDetector.breakEventMutable)
   {
-    irDetector->lastPersistant = irDetector->currentPersistant;
-    irDetector->currentPersistant = true;
+    irDetector.lastPersistant = irDetector.currentPersistant;
+    irDetector.currentPersistant = true;
+    irDetector.breakEventMutable = false;
   }
-  irDetector->lastRead = irDetector->currentRead;
-  irDetector->currentRead = v;
+  irDetector.lastRead = irDetector.currentRead;
+  irDetector.currentRead = v;
 }
 
-inline bool readTouch(byte pin)
-{
-  /*
-  Get logic corrected digital read for touch input pin
-  */
-  bool v = digitalRead(pin);
-  return TOUCH_ACTIVE_LOW ? !v : v;
-}
-
-void initTouch(TouchState *touchSensor, byte pin, byte side)
+void initTouch(TouchState &touchSensor, byte pin, byte side)
 {
   /*
   Init function to initialize touchSensor with default parameters
@@ -132,16 +232,16 @@ void initTouch(TouchState *touchSensor, byte pin, byte side)
   <byte> side : side identifier
   */
   pinMode(pin, INPUT_PULLUP);
-  touchSensor->pin = pin;
-  touchSensor->side = side;
-  touchSensor->current = readTouch(touchSensor->pin);
-  touchSensor->last = false;
-  touchSensor->inTouch = false;
-  touchSensor->touchEvent = false;
-  touchSensor->clearEvent = true;
+  touchSensor.pin = pin;
+  touchSensor.side = side;
+  touchSensor.current = digitalReadCorrected(touchSensor.pin, TOUCH_ACTIVE_LOW);
+  touchSensor.last = false;
+  touchSensor.inTouch = false;
+  touchSensor.touchEvent = false;
+  touchSensor.clearEvent = true;
 }
 
-void detectTouch(TouchState *touchSensor, unsigned long tNow, unsigned long tRuntimeStart)
+void detectTouch(TouchState &touchSensor, unsigned long tNow)
 {
   /*
   Function to detect touchSensor state changes and update state parameters accordingly
@@ -149,30 +249,30 @@ void detectTouch(TouchState *touchSensor, unsigned long tNow, unsigned long tRun
   <unsigned long> tNow : current time of execution
   <unsigned long> tRuntimeStart : time of runtime start
   */
-  bool v = readTouch(touchSensor->pin);
+  bool v = digitalReadCorrected(touchSensor.pin, TOUCH_ACTIVE_LOW);
 
-  if (v && !touchSensor->last)
+  if (v && !touchSensor.last)
   {
     // add state change as and when needed for duration dependent reward release
-    touchSensor->tStart = tNow;
-    touchSensor->inTouch = true;
-    touchSensor->touchEvent = true;
-    touchSensor->clearEvent = false;
+    touchSensor.tStart = tNow;
+    touchSensor.inTouch = true;
+    touchSensor.touchEvent = true;
+    touchSensor.clearEvent = false;
     // log
-    eventLog(touchSensor->side, TOUCH, ON, tNow - tRuntimeStart);
+    eventLog(touchSensor.side, TOUCH, ON, tNow);
   }
-  else if (!v && touchSensor->last)
+  else if (!v && touchSensor.last)
   {
-    touchSensor->inTouch = false;
-    touchSensor->clearEvent = true;
-    touchSensor->touchEvent = false;
+    touchSensor.inTouch = false;
+    touchSensor.clearEvent = true;
+    touchSensor.touchEvent = false;
     // log
-    eventLog(touchSensor->side, TOUCH, OFF, tNow - tRuntimeStart);
+    eventLog(touchSensor.side, TOUCH, OFF, tNow);
   }
-  touchSensor->last = v;
+  touchSensor.last = v;
 }
 
-void initSolenoid(SolenoidState *solenoidValve, byte pin, byte side)
+void initSolenoid(SolenoidState &solenoidValve, byte pin, byte side)
 {
   /*
   Init function to initialize solenoidState with default parameters
@@ -181,11 +281,11 @@ void initSolenoid(SolenoidState *solenoidValve, byte pin, byte side)
   <byte> side : side identifier
   */
   pinMode(pin, OUTPUT);
-  digitalWrite(pin, SOLENOID_ACTIVE_HIGH ? LOW : HIGH);
-  solenoidValve->open = false;
+  digitalWriteCorrected(pin, OFF, SOLENOID_ACTIVE_HIGH);
+  solenoidValve.open = false;
 }
 
-void activateSolenoid(SolenoidState *solenoidValve, unsigned long duration, unsigned long tNow, unsigned long tRuntimeStart)
+void activateSolenoid(SolenoidState &solenoidValve, unsigned long tNow, unsigned long duration = SOLENOID_DURATION)
 {
   /*
   Function to activate solenoid valve and update state parameters accordingly
@@ -194,23 +294,23 @@ void activateSolenoid(SolenoidState *solenoidValve, unsigned long duration, unsi
   <unsigned long> tNow : current time of execution
   <unsigned long> tRuntimeStart : time of runtime start
   */
-  if (solenoidValve->open)
+  if (solenoidValve.open)
   {
     return;
   }
-  if (!solenoidValve->open)
+  if (!solenoidValve.open)
   {
-    solenoidValve->open = true;
-    solenoidValve->tOpen = tNow;
-    solenoidValve->duration = duration;
-    digitalWrite(solenoidValve->pin, SOLENOID_ACTIVE_HIGH ? HIGH : LOW);
+    solenoidValve.open = true;
+    solenoidValve.tOpen = tNow;
+    solenoidValve.duration = duration;
+    digitalWriteCorrected(solenoidValve.pin, ON, SOLENOID_ACTIVE_HIGH);
 
     // log
-    eventLog(solenoidValve->side, SOLENOID, ON, tNow - tRuntimeStart);
+    eventLog(solenoidValve.side, SOLENOID, ON, tNow);
   }
 }
 
-void updateSolenoid(SolenoidState *solenoidValve, unsigned long tNow, unsigned long tRuntimeStart)
+void updateSolenoid(SolenoidState &solenoidValve, unsigned long tNow)
 {
   /*
   Function to check for duration elapsed since solenoid valve activation and update state parameters accordingly
@@ -218,14 +318,14 @@ void updateSolenoid(SolenoidState *solenoidValve, unsigned long tNow, unsigned l
   <unsigned long> tNow : current time of execution
   <unsigned long> tRuntimeStart : time of runtime start
   */
-  if (solenoidValve->open && tNow - solenoidValve->tOpen >= solenoidValve->duration)
+  if (solenoidValve.open && tNow - solenoidValve.tOpen >= solenoidValve.duration)
   {
-    solenoidValve->open = false;
-    solenoidValve->tClose = tNow;
-    digitalWrite(solenoidValve->pin, SOLENOID_ACTIVE_HIGH ? LOW : HIGH);
+    solenoidValve.open = false;
+    solenoidValve.tClose = tNow;
+    digitalWriteCorrected(solenoidValve.pin, OFF, SOLENOID_ACTIVE_HIGH);
 
     // log
-    eventLog(solenoidValve->side, SOLENOID, OFF, tNow - tRuntimeStart);
+    eventLog(solenoidValve.side, SOLENOID, OFF, tNow);
   }
 }
 
--- linear_track_alternate_reward.ino
+++ linear_track_alternate_reward.ino
@@ -2,15 +2,8 @@
 #include "data.h"
 #include "helper.h"
 
-
-bool runTimeFlag;
-bool ledBlink;
-
-unsigned long tNow;
-unsigned long tStart;
-unsigned long tRuntimeStart;
-unsigned long tLedBlinkOn;
-
+RuntimeState runtime;
+BlinkLEDState ledA;
 IRState irDetectorA, irDetectorB;
 TouchState touchSensorA, touchSensorB;
 SolenoidState solenoidValveA, solenoidValveB;
@@ -18,107 +11,61 @@ SolenoidState solenoidValveA, solenoidValveB;
 void setup()
 {
   Serial.begin(BAUD_RATE);
-  delay(1001); // to allow serial to sync
-
-  ledBlink = false;
-  runTimeFlag = false;
-  initIR(&irDetectorA, IR_A_PIN, SIDE_A);
-  initIR(&irDetectorB, IR_B_PIN, SIDE_B);
-  initTouch(&touchSensorA, TOUCH_A_PIN, SIDE_A);
-  initTouch(&touchSensorB, TOUCH_B_PIN, SIDE_B);
-  initSolenoid(&solenoidValveA, SOLENOID_A_PIN, SIDE_A);
-  initSolenoid(&solenoidValveB, SOLENOID_B_PIN, SIDE_B);
-  digitalWrite(LED_RUNTIME, LOW);
+  delay(1001); // to allow serial conenction to be established
+  
+  initRuntime(runtime, LED_RUNTIME);
+  initBlinkLED(ledA, LED_BLINK_PIN, SIDE_A);
+  initIR(irDetectorA, IR_A_PIN, SIDE_A);
+  initIR(irDetectorB, IR_B_PIN, SIDE_B);
+  initTouch(touchSensorA, TOUCH_A_PIN, SIDE_A);
+  initTouch(touchSensorB, TOUCH_B_PIN, SIDE_B);
+  initSolenoid(solenoidValveA, SOLENOID_A_PIN, SIDE_A);
+  initSolenoid(solenoidValveB, SOLENOID_B_PIN, SIDE_B);
+  initRuntime(runtime, LED_RUNTIME);
 
   // log
   Serial.print("Linear Track Behaviour in mode: ");
   OPERATION_MODE ? Serial.println("Mode_B") : Serial.println("Mode_A");
-
-  tStart = currentTime();
 }
 
 void loop()
 {
-  tNow = currentTime();
-  if (tNow - tRuntimeStart >= RUN_TIME)
+  updateRuntime(runtime);
+  
+  if (runtime.runTimeFlag)
   {
-    digitalWrite(LED_RUNTIME, LOW);
-    runTimeFlag = 0;
+    detectIR(irDetectorA, runtime.tNow);
+    detectIR(irDetectorB, runtime.tNow);
+    detectTouch(touchSensorA, runtime.tNow);
+    detectTouch(touchSensorB, runtime.tNow);
 
-    // log
-    Serial.print('E');
-    Serial.println(tNow);
+    updateBlinkLED(ledA, runtime.tNow);
+    updateSolenoid(solenoidValveA, runtime.tNow);
+    updateSolenoid(solenoidValveB, runtime.tNow);
 
-    while (1)
-      ;
+    switch (OPERATION_MODE)
+    {
+      case MODE_A:
+        if (irDetectorA.currentPersistant && irDetectorB.lastPersistant)
+        {
+          activateSolenoid(solenoidValveA, runtime.tNow);
+          activateSolenoid(solenoidValveB, runtime.tNow);//ensure the reservoir inlet valve is closed
+          irDetectorB.lastPersistant = false;
+          irDetectorB.currentPersistant = false;
+        }
+        break;
+      case MODE_B:
+        if (irDetectorB.currentPersistant && irDetectorA.lastPersistant)
+        {
+          activateSolenoid(solenoidValveB, runtime.tNow);
+          activateSolenoid(solenoidValveA, runtime.tNow);//ensure the reservoir inlet valve is closed
+          irDetectorA.lastPersistant = false;
+          irDetectorA.currentPersistant = false;
+        }
+      default:
+        Serial.println("Operation Mode configuration incorrect/incomplete");
+        break;
+    }
   }
-
-  if (!runTimeFlag && tNow - tStart >= DELAY_START)
-  {
-    runTimeFlag = 1;
-    digitalWrite(LED_RUNTIME, HIGH);
-    tRuntimeStart = tNow;
-    digitalWrite(LED_BLINK_PIN, HIGH);
-    tLedBlinkOn = tNow;
-    ledBlink = true;
-
-    // log
-    Serial.print('S');
-    Serial.println(tRuntimeStart);
-  }
-
-  if (runTimeFlag)
-  {
-
-    detectIR(&irDetectorA, IR_A_PIN, SIDE_A);
-    detectIR(&irDetectorB, IR_B_PIN, SIDE_B);
-    detectTouch(&touchSensorA, TOUCH_A_PIN, SIDE_A);
-    detectTouch(&touchSensorB, TOUCH_B_PIN, SIDE_B);
-    if (ledBlink && tNow - tLedBlinkOn > LED_BLINK_A_INTERVAL)
-    {
-      digitalWrite(LED_BLINK_PIN, LOW);
-      tLedBlinkOn = tNow;
-      ledBlink = false;
-    }
-    if (!ledBlink && tNow - tLedBlinkOn > LED_BLINK_A_INTERVAL)
-    {
-      digitalWrite(LED_BLINK_PIN, HIGH);
-      tLedBlinkOn = tNow;
-      ledBlink = true;
-    }
-
-    if (irDetectorA.breakEventMutable)
-    {
-      irDetectorB.currentPersistant = false;
-      irDetectorA.breakEventMutable = false;
-    }
-    if (irDetectorB.breakEventMutable)
-    {
-      irDetectorA.currentPersistant = false;
-      irDetectorB.breakEventMutable = false;
-    }
-
-    if (OPERATION_MODE == MODE_A && irDetectorA.currentPersistant && irDetectorB.lastPersistant)
-    {
-      activateSolenoid(&solenoidValveA, SOL_DEFAULT, tNow, tRuntimeStart);
-      // activateSolenoid(solB, SOL_B_PIN, SOL_DEFAULT_MS, SIDE_B);//ensure the reservoir inlet valve is closed
-      updateSolenoid(&solenoidValveA, tNow, tRuntimeStart);
-      updateSolenoid(&solenoidValveB, tNow, tRuntimeStart);
-      irDetectorB.lastPersistant = false;
-      return;
-    }
-    if (OPERATION_MODE == MODE_B && irDetectorB.currentPersistant && irDetectorA.lastPersistant)
-    {
-
-      activateSolenoid(&solenoidValveB, SOL_DEFAULT, tNow, tRuntimeStart);
-      // activateSolenoid(solA, SOL_A_PIN, SOL_DEFAULT_MS, SIDE_A);//ensure the reservoir inlet valve is closed
-      updateSolenoid(&solenoidValveA, tNow, tRuntimeStart);
-      updateSolenoid(&solenoidValveB, tNow, tRuntimeStart);
-      irDetectorA.lastPersistant = false;
-      return;
-    }
-
-    updateSolenoid(&solenoidValveA, tNow, tRuntimeStart);
-    updateSolenoid(&solenoidValveB, tNow, tRuntimeStart);
-  }
+  runtime.tLast = runtime.tNow;
 }
