9449e77ca2615879bca57ecc8871271fdda19caf
shreyasmiraj <shreyasmiraj8@gmail.com>
2025-12-30 16:06:20 +0530

    added TTL logic and removed redundant code

--- linear_track_alternate_reward.ino
+++ linear_track_alternate_reward.ino
@@ -3,7 +3,9 @@
 #include "helper.h"
 
 int lastIR;
+unsigned long lastTTL = 0;
 
+TTLState inputTrigger, outputTrigger, outputIR, outputTouch, outputSolenoid;
 RuntimeState runtime;
 BlinkLEDState ledA;
 IRState irDetectorA, irDetectorB;
@@ -14,24 +16,35 @@ void setup()
 {
   lastIR = -1;
   Serial.begin(BAUD_RATE);
-  initRuntime(runtime, LED_RUNTIME);
   delay(1001); // to allow serial conenction to be established
+  initTTL(inputTrigger, INPUT_TRIGGER, INPUT);
+  initTTL(outputTrigger, OUTPUT_TRIGGER, OUTPUT);
+  initTTL(outputIR, OUTPUT_IR, OUTPUT);
+  initTTL(outputTouch, OUTPUT_TOUCH, OUTPUT);
+  initTTL(outputSolenoid, OUTPUT_SOLENOID, OUTPUT);
+  initRuntime(runtime, LED_RUNTIME, &outputTrigger, &inputTrigger);
   initBlinkLED(ledA, LED_BLINK_PIN, SIDE_A);
-  initIR(irDetectorA, IR_A_PIN, SIDE_A);
-  initIR(irDetectorB, IR_B_PIN, SIDE_B);
-  initTouch(touchSensorA, TOUCH_A_PIN, SIDE_A);
-  initTouch(touchSensorB, TOUCH_B_PIN, SIDE_B);
-  initSolenoid(solenoidValveA, SOLENOID_A_PIN, SIDE_A);
-  initSolenoid(solenoidValveB, SOLENOID_B_PIN, SIDE_B);
+  initIR(irDetectorA, IR_A_PIN, SIDE_A, IR_A_INDICATOR, &outputIR, TTL_PULSE_PERIOD);
+  initIR(irDetectorB, IR_B_PIN, SIDE_B, IR_B_INDICATOR, &outputIR, TTL_PULSE_PERIOD / 2);
+  initTouch(touchSensorA, TOUCH_A_PIN, SIDE_A, &outputTouch, TTL_PULSE_PERIOD);
+  initTouch(touchSensorB, TOUCH_B_PIN, SIDE_B, &outputTouch, TTL_PULSE_PERIOD / 2);
+  initSolenoid(solenoidValveA, SOLENOID_A_PIN, SIDE_A, &outputSolenoid, TTL_PULSE_PERIOD);
+  initSolenoid(solenoidValveB, SOLENOID_B_PIN, SIDE_B, &outputSolenoid, TTL_PULSE_PERIOD / 2);
   // log
   Serial.print("Linear Track Behaviour in mode: ");
   OPERATION_MODE ? Serial.println("Mode_B") : Serial.println("Mode_A");
 }
 
 void loop()
-{
-  updateRuntime(runtime);
-  if (runtime.runTimeFlag)
+{ 
+  
+  updateRuntime(runtime); //inputTrigger detectTTL is interlocked with updateRuntime due to its interdependency
+                          //inputTrigger detect state is stored in inputTrigger.detect as boolean.
+  updateTTL(outputTrigger, runtime.tNow);
+  updateTTL(outputIR, runtime.tNow);
+  updateTTL(outputTouch, runtime.tNow);
+  updateTTL(outputSolenoid, runtime.tNow);
+  if (runtime.runtimeFlag)
   { 
     detectIR(irDetectorA, runtime.tNow);
     detectIR(irDetectorB, runtime.tNow);
@@ -42,15 +55,13 @@ void loop()
     updateSolenoid(solenoidValveA, runtime.tNow);
     updateSolenoid(solenoidValveB, runtime.tNow);
 
-    
-
     switch (OPERATION_MODE)
     {
       case MODE_A:
         if (irDetectorA.currentPersistant && (lastIR == SIDE_B))
         {
           activateSolenoid(solenoidValveA, runtime.tNow);
-          activateSolenoid(solenoidValveB, runtime.tNow);//ensure the reservoir inlet valve is closed
+          // activateSolenoid(solenoidValveB, runtime.tNow);//ensure the reservoir inlet valve is closed
         }
         break;
       case MODE_B:
@@ -63,20 +74,15 @@ void loop()
         Serial.println("Operation Mode configuration incorrect/incomplete");
         break;
     }
-    if (irDetectorA.currentPersistant) 
+    if (irDetectorA.breakEventMutable) 
     {
-      irDetectorA.lastPersistant = irDetectorA.currentPersistant;
-      irDetectorA.currentPersistant = false;
-      irDetectorB.lastPersistant = false;
+      irDetectorA.breakEventMutable = false;
       lastIR = SIDE_A;
     }
-    else if (irDetectorB.currentPersistant)
+    else if (irDetectorB.breakEventMutable)
     {
-      irDetectorB.lastPersistant = irDetectorB.currentPersistant;
-      irDetectorB.currentPersistant = false;
-      irDetectorA.lastPersistant = false;
+      irDetectorB.breakEventMutable = false;
       lastIR = SIDE_B;
     }
   }
-  runtime.tLast = runtime.tNow;
 }
